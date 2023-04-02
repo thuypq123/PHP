d07ed6142137d22f45c1f8224f986b40b473b0f6
thuypq123 <lmthuy37@gmail.com>
2023-04-03 02:37:21 +0700

    ADDADD profile

--- app/models/paymentUserModel.php
+++ app/models/paymentUserModel.php
@@ -6,8 +6,8 @@
     class paymentUserModel{
         public function addPayment($MaKhachHang, $SDT, $DiaChi){
             $conn = Database::getInstance();
-            // find MaHoaDon by MaKhachHang and ThanhToan = 0
-            $query = "SELECT MaHoaDon FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND ThanhToan = 0";
+            // find MaHoaDon by MaKhachHang and TinhTrang = 0
+            $query = "SELECT MaHoaDon FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND TinhTrang = 0";
             $result = mysqli_query($conn, $query);
             $row = mysqli_fetch_assoc($result);
             // sum chitetHoaDon by MaHoaDon
@@ -16,16 +16,16 @@
             $rowCTHD = mysqli_fetch_assoc($result);
             $TongTien = $rowCTHD["TongTien"];
             // update HoaDon with SDT, DiaChi 
-            $query = "UPDATE HoaDon SET SDT = '$SDT', DiaChi = '$DiaChi', TongTien = '$TongTien' WHERE MaKhachHang = '$MaKhachHang' AND ThanhToan = 0";
+            $query = "UPDATE HoaDon SET SDT = '$SDT', DiaChi = '$DiaChi', TongTien = '$TongTien' WHERE MaKhachHang = '$MaKhachHang' AND TinhTrang = 0";
             $result = mysqli_query($conn, $query);
-            // update HoaDon with ThanhToan = 1
-            $query = "UPDATE HoaDon SET ThanhToan = 1 WHERE MaKhachHang = '$MaKhachHang' AND ThanhToan = 0";
+            // update HoaDon with TinhTrang = 1
+            $query = "UPDATE HoaDon SET TinhTrang = 1 WHERE MaKhachHang = '$MaKhachHang' AND TinhTrang = 0";
             $result = mysqli_query($conn, $query);
-            // create new HoaDon with ThanhToan = 0 by MaKhachHang
-            $query = "INSERT INTO HoaDon(MaKhachHang, ThanhToan) VALUES ('$MaKhachHang', 0)";
+            // create new HoaDon with TinhTrang = 0 by MaKhachHang
+            $query = "INSERT INTO HoaDon(MaKhachHang, TinhTrang) VALUES ('$MaKhachHang', 0)";
             $result = mysqli_query($conn, $query);
             // set session by MaHoaDon
-            $query = "SELECT MaHoaDon FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND ThanhToan = 0";
+            $query = "SELECT MaHoaDon FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND TinhTrang = 0";
             $result = mysqli_query($conn, $query);
             $row = mysqli_fetch_assoc($result);
             $_SESSION["MaHoaDon"] = $row["MaHoaDon"];
@@ -35,33 +35,27 @@
         }
         public function getPaymentList($MaKhachHang){
             $conn = Database::getInstance();
-            $query = "SELECT * FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND ThanhToan = 1";
+            $query = "SELECT * FROM hoadon WHERE MaKhachHang = '$MaKhachHang' AND TinhTrang = 1";
             $result = mysqli_query($conn, $query);
             $data = array();
             while ($row = mysqli_fetch_assoc($result)) {
                 $data[] = $row;
             }
             for($i = 0; $i < count($data); $i++) {
-                // $query = "SELECT * FROM chitiethoadon WHERE MaHoaDon = ".$data[$i]["MaHoaDon"]."";
-                $query = "SELECT * FROM chitiethoadon WHERE MaHoaDon = '15'";
+                $query = "SELECT * FROM chitiethoadon WHERE MaHoaDon = ".$data[$i]["MaHoaDon"]."";
                 $resultCTHD = mysqli_query($conn, $query);
-                $row = mysqli_fetch_array($resultCTHD);
-                header('Content-Type: application/json; charset=utf-8');
-                echo json_encode($row);
-            // loop data and get sanpham by MaHoaDon from chitiethoadon and sanpham table
-            // while ($row = mysqli_fetch_assoc($result)){
-            //     $query = "SELECT * FROM chitiethoadon WHERE MaHoaDon = '$row[MaHoaDon]'";
-            //     $resultCTHD = mysqli_query($conn, $query);
-            //     $rowCTHD = mysqli_fetch_assoc($resultCTHD);            
-                // while($rowCTHD = mysqli_fetch_assoc($resultCTHD)){
-                //     $query = "SELECT * FROM sanpham WHERE MaSanPham = '$rowCTHD[MaSanPham]'";
-                //     $resultSP = mysqli_query($conn, $query);
-                //     $rowSP = mysqli_fetch_assoc($resultSP);
-                //     $rowCTHD["SanPham"] = $rowSP;
-                // }
-                // $row["ChiTietHoaDon"] = $rowCTHD;
-                // $data[] = $row;
+                $row = mysqli_fetch_all($resultCTHD, MYSQLI_ASSOC);
+                for($j = 0; $j < count($row); $j++) {
+                    $query = "SELECT * FROM sanpham WHERE MaSanPham = '".$row[$j]["MaSanPham"]."'";
+                    $resultSP = mysqli_query($conn, $query);
+                    $rowSP = mysqli_fetch_assoc($resultSP);
+                    $row[$j]["TenSanPham"] = $rowSP["TenSanPham"];
+                    $row[$j]["HinhAnh"] = $rowSP["AnhSanPham"];
+                    $row[$j]["Gia"] = $rowSP["GiaSanPham"];
+                    $data[$i]["ChiTietHoaDon"] = $row;
+                }
             } 
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($data);
         }
     }
-?>
--- app/views/profile.php
+++ app/views/profile.php
@@ -25,58 +25,12 @@ require('./app/views/layout/navbar.php');
             </div>
         </div>
         <!--List Crad Shopping-->
-        <div class="col-md-6 shadow-4-strong">
+        <div class="col-md-7 shadow-4-strong">
             <div class="title d-flex justify-content-center text-black-50">
                 <h2>Danh Sách Đơn Hàng</h2>
             </div>
-            <div class="accordion accordion-borderless" id="accordionFlushExampleX" style="width:100%; overflow: auto; height: 60vh;">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingOneX">
-                        <button class="accordion-button" type="button" data-mdb-toggle="collapse" data-mdb-target="#flush-collapseOneX" aria-expanded="true" aria-controls="flush-collapseOneX">
-                            Đơn Hàng #1
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOneX" class="accordion-collapse collapse show" aria-labelledby="flush-headingOneX" data-mdb-parent="#accordionFlushExampleX">
-                        <table class="table align-middle mb-0 bg-white ">
-                            <thead class="bg-light border border-white">
-                                <tr>
-                                    <th>Tên Sản Phẩm</th>
-                                    <th>Số Lượng</th>
-                                    <th>Giá</th>
-                                    <th>Tổng</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 45px; height: 45px" class="rounded-circle" />
-                                            <div class="ms-3">
-                                                <p class="fw-bold mb-1">Ai Phôn Mười Bốn Bờ Rồ Măc</p>
-                                                <p class="text-muted mb-0">john.doe@gmail.com</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="fw-normal mb-1">20</p>
-                                    </td>
-                                    <td>
-                                        <span class="badge badge-primary rounded-pill d-inline">1000000</span>
-                                    </td>
-                                    <td>
-                                    <span class="badge badge-success rounded-pill d-inline">1000000</span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!--Total Price-->
-                    <div class="container text-muted d-flex justify-content-end" style="margin: 20px 0">
-                        <h5 style="margin: 0 20px">Tổng Cộng:</h5>
-                        <h5>100000</h5>
-                    </div>
-                    <hr>
-                    </div>
-                </div>
+            <div class="accordion accordion-borderless" id="renderList" style="width:100%; overflow: auto; height: 60vh;">
+                <!--Render List-->
             </div>
         </div>
     </div>
